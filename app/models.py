272cd48535f255f5577b36c80fec78bd8d3fa3c4
Robert Schulze <robert_schulze@posteo.de>
2019-04-07 20:56:53 +0200

    removed commented out code fragments

--- app/models.py
+++ app/models.py
@@ -1,16 +1,3 @@
 ''' This file includes all the functions needed for the app '''
 
 from jinja2 import Markup, PackageLoader, Environment
-
-#def file_include(name):
-    #return Markup(loader.get_source(env, name)[0])
-#
-#loader = PackageLoader(__name__, '')
-#env = Environment(loader=loader)
-#env.globals['file_include'] = file_include
-#
-#def render():
-#    return env.get_template('base.html').render()
-#
-#if __name__ == '__main__':
-#    print(render())
